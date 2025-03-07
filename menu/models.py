b81ce2f3c534ae3b4c0a6a8ecc0c3b78de583fdc
Heba Ghazali <43419199+hebaghazali@users.noreply.github.com>
2025-03-07 15:10:33 +0200

    feat: add chef field to FoodItem model and update menu list template to display chef name

--- menu/models.py
+++ menu/models.py
@@ -5,6 +5,7 @@ class FoodItem(models.Model):
     description = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=5, decimal_places=2)
     image_url = models.URLField(blank=True, null=True) 
+    chef = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.name
