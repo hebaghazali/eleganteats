d3302d128fdf5f5413a128d97f4df9815ffec642
Heba Ghazali <43419199+hebaghazali@users.noreply.github.com>
2025-03-07 08:23:54 +0200

    feat: add item detail view and corresponding URL pattern

--- menu/views.py
+++ menu/views.py
@@ -4,4 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the menu index.")
-    
+
+def item(request, item_id):
+    return HttpResponse(f"You're looking at item <strong>{item_id}</strong>.")
