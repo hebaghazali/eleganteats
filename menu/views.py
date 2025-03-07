fb23c1d7947144db23f618ce3795e019fb6b7105
Heba Ghazali <43419199+hebaghazali@users.noreply.github.com>
2025-03-07 11:25:03 +0200

    feat: register Item model in admin and update views for item listing and detail

--- menu/views.py
+++ menu/views.py
@@ -1,9 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from menu.models import Item
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the menu index.")
+    item_list = Item.objects.all()
+    print(f"{item_list}")
+    return HttpResponse(item_list)
 
 def item(request, item_id):
-    return HttpResponse(f"You're looking at item <strong>{item_id}</strong>.")
+    try:
+        item = Item.objects.get(pk=item_id)
+        if not item:
+            return HttpResponse("Item not found")
+        return HttpResponse(f"Item: {item.name} - {item.description} - ${item.price}")
+    except Item.DoesNotExist:
+        return HttpResponse("Failed: Item not found")
