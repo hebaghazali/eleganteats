987e9c3fe82a9b85318fe68fe802b0c488d4e9a3
hebaghazali <hebbaghazali@gmail.com>
2025-03-09 15:31:13 +0200

    feat: set default image URL and alter image_url field

--- menu/models.py
+++ menu/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
+
 class FoodItem(models.Model):
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=5, decimal_places=2)
-    image_url = models.URLField(blank=True, null=True) 
+    image_url = models.URLField(
+        blank=True,
+        default="https://t3.ftcdn.net/jpg/02/48/42/64/360_F_248426448_NVKLywWqArG2ADUxDq6QprtIzsF82dMF.jpg",
+    )
     chef = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
