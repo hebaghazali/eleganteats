192948752b5eb8f75fbffc308d8d7a575c2fff1e
Heba Ghazali <43419199+hebaghazali@users.noreply.github.com>
2025-04-11 18:58:33 +0200

    feat: implement user registration with validation and success message

--- users/views.py
+++ users/views.py
@@ -1,12 +1,23 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
+from .forms import RegisterForm
+from django.contrib.auth import login
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'The user has been created successfully.')
+            return redirect('menu:menu_list')
+
+    else:
+        form = RegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
 
-def login(request):
-    form = AuthenticationForm()
-    return render(request, 'users/login.html', {'form': form})
+# def login(request):
+#     form = AuthenticationForm()
+#     return render(request, 'users/login.html', {'form': form})
