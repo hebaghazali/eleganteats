07bafb195b6db00ae85ab337997a04d30e65c135
Heba Ghazali <43419199+hebaghazali@users.noreply.github.com>
2025-04-12 21:38:57 +0200

    feat: enhance profile page with profile picture styling; update CSS for menu item images

--- users/urls.py
+++ users/urls.py
@@ -15,5 +15,4 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
